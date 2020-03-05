b61f4cffc18d5f1966ea133ef1d9f7672b97566e
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-03-05 08:29:16 +0100

    initial version of vitrite package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://vitrite.vanmiddlesworth.org/vitrite/1.1.1/Vitrite-1.1.zip'
+$unzipLocation = "$(Join-Path $toolsDir Vitrite)"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $unzipLocation
+  url           = $url
+  checksum      = '870BDFDDDB463587B98CCE6269DD5CDA3335CB6004358DCFC226BBC7CA625CC6'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
